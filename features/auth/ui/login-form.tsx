82277d93fe9b33d52dad54151b8bea6942e9f161
juliagrandmontagne <j.gmt@outlook.fr>
2025-11-04 16:36:25 +0100

    login

--- features/auth/ui/login-form.tsx
+++ features/auth/ui/login-form.tsx
@@ -1,16 +1,41 @@
+"use client";
+import { BlablagnoleLogo } from "@/components/ui/blablagnole-logo";
 import { Button } from "@/components/ui/button";
 import {
   Field,
   FieldDescription,
+  FieldError,
   FieldGroup,
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import InputPassword from "@/components/ui/input-password";
 import Link from "next/link";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema, loginSchema } from "@/schemas/login";
+
 
 export function LoginForm() {
+
+
+
+    const form = useForm<LoginSchema>({
+      resolver: zodResolver(loginSchema),
+      defaultValues: {
+        email: "",
+        password: "",
+      },
+    });
+  
+    const onSubmit = (data: LoginSchema) => {
+      console.log(data);
+    };
   return (
-    <form className="p-6 md:p-8 my-auto">
+    <form className="p-6 md:p-8 my-auto"
+    id="login-form"
+       onSubmit={form.handleSubmit(onSubmit)}>
+        <BlablagnoleLogo className="w-56 mx-auto mb-8" />
       <FieldGroup>
         <div className="flex flex-col items-center gap-2 text-center">
           <h1 className="text-2xl font-bold">Connexion</h1>
@@ -18,32 +43,49 @@ export function LoginForm() {
             Connectez-vous à votre compte
           </p>
         </div>
-        <Field>
-          <FieldLabel htmlFor="email">Email</FieldLabel>
-          <Input
-            className="border"
-            id="email"
-            type="email"
-            placeholder="m@example.com"
-            required
-          />
-        </Field>
-        <Field>
-          <div className="flex items-center">
-            <FieldLabel htmlFor="password">Mot de passe</FieldLabel>
-            <Link
-              href="#"
-              className="ml-auto text-sm underline-offset-2 hover:underline"
-            >
-              Mot de passe oublié ?
-            </Link>
-          </div>
-          <Input id="password" type="password" required />
-       
-        </Field>
+       <Controller
+          control={form.control}
+          name="email"
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+              <Input
+                {...field}
+                id={field.name}
+                aria-invalid={fieldState.invalid}
+                type="email"
+                placeholder="m@example.com"
+                autoComplete="email"
+                required
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+
+        <Controller
+                 control={form.control}
+                 name="password"
+                 render={({ field, fieldState }) => (
+                   <Field data-invalid={fieldState.invalid}>
+                     <FieldLabel htmlFor={field.name}>
+                       Confirmation du mot de passe
+                     </FieldLabel>
+                     <InputPassword
+                       {...field}
+                       id={field.name}
+                       aria-invalid={fieldState.invalid}
+                       placeholder="Entrez votre mot de passe"
+                       autoComplete="password"
+                       required
+                     />
+                     {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                   </Field>
+                 )}
+               />
         
         <Field>
-          <Button type="submit">Connexion</Button>
+          <Button type="submit" form="login-form" >Connexion</Button>
         </Field>
 
         <FieldDescription className="text-center">
@@ -54,3 +96,10 @@ export function LoginForm() {
     </form>
   );
 }
+
+ const onSubmit = (data: LoginSchema) => {
+    console.log("Connexion avec :", data);
+    // 👉 Ici, tu pourras ajouter ton appel API ou ta logique d'authentification
+  };
+
+ 
--- shemas/login.ts
+++
@@ -1,9 +0,0 @@
-
-import { z } from "zod";
-
-export const loginSchema = z.object({ 
-  email: z.email({message: "L'adresse e-mail est invalide."}),
-   password: z.string({ message: "Le mot de passe est requis" }).min(8, { message: "Le mot de passe doit contenir au moins 8 caractères" }),
-});
-
-
