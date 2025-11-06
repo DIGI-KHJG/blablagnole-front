c6118f62bcc7e7a38c2b228ebc547958582ce830
juliagrandmontagne <j.gmt@outlook.fr>
2025-11-06 16:56:04 +0100

    type carpooling + adress

--- types/carpooling.ts
+++ types/carpooling.ts
@@ -1,15 +1,25 @@
-import { Address } from "cluster";
-import { Timestamp } from "next/dist/server/lib/cache-handlers/types";
+import { User } from "./user";
+
 
 export type Carpooling = {
   id: string;
-  driverId:string;
-  departure_Date:string ;
-  departure_Location: Address;
-  arrival_Location: string;
+  driver: User;
+  departure_date: string ;
+  departure_location: Address;
+  arrival_location: string;
   time: number;
-  available_Seats: number;
-  lenght: number;
-  vehicle: vehicle;
-    };
+  available_seats: number;
+  distance : number;
+  car: Car;
+  carpooling_status:carpooling_status
+  passengers: User[];
+};
 
+export type carpooling_status = "ACTIVE" | "COMPLETED"| "CANCELED";
+
+export type Address = {
+  street: string;
+  city: string;
+  zipCode: string;
+  country: string;
+};
